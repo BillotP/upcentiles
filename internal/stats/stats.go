6a8a1ac8d8a0b120afa0262666b8f3d6fbaa9a88
ravachol nildock <ravacholnildock@devnull.mail>
2023-09-13 08:49:10 +0200

    refacto(stats): separation of concerns

--- internal/stats/stats.go
+++ internal/stats/stats.go
@@ -1,35 +1,23 @@
 package stats
 
-import (
-	"upcentile/internal/api"
-	"upcentile/internal/upfluence"
+import "fmt"
 
-	"golang.org/x/exp/slices"
-)
+// Percentiles compute the percentiles  p_50, p_90 and p_99 values for a values slice.
+func Percentiles(verbose bool, values []int) [3]int {
+	Quicksort(values)
 
-// Percentiles compute the percentiles  p_50, p_90 and p_99 values for a particular AnalysisDimension of StreamEvent datas.
-func Percentiles(dimension api.AnalysisDimension, datas []*upfluence.StreamEvent) [3]uint64 {
-	var values = []uint64{}
-
-	for _, el := range datas {
-		switch dimension {
-		case api.Comments:
-			values = append(values, el.Comments())
-		case api.Favorites:
-			values = append(values, el.Favorites())
-		case api.Retweets:
-			values = append(values, el.Retweets())
-
-		default:
-			values = append(values, el.Likes())
-		}
+	if verbose {
+		fmt.Printf("[INFO] Sorted values : %+v\n", values)
 	}
-	// If no lib allowed, sort might be implement using a partition sort for small list,
-	// and quicksort for longest
-	slices.Sort(values)
-	var out = [3]uint64{}
-	out[0] = values[int(float64(50.0/100)*float64(len(datas)-1))]
-	out[1] = values[int(float64(90.0/100)*float64(len(datas)-1))]
-	out[2] = values[int(float64(99.0/100)*float64(len(datas)-1))]
+
+	var out = [3]int{}
+	out[0] = values[int(float64(50.0/100)*float64(len(values)-1))]
+	out[1] = values[int(float64(90.0/100)*float64(len(values)-1))]
+	out[2] = values[int(float64(99.0/100)*float64(len(values)-1))]
+
+	if verbose {
+		fmt.Printf("[INFO] Found percentiles %+v\n", out)
+	}
+
 	return out
 }
