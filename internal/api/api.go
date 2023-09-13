6fdcd6c21960f78ea151f7f0552d455745a7b911
ravachol nildock <ravacholnildock@devnull.mail>
2023-09-13 08:46:05 +0200

    feat(api): Add more infos for api consumer

--- internal/api/api.go
+++ internal/api/api.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"fmt"
+	"strings"
 	"time"
 )
 
@@ -18,10 +19,23 @@ const (
 	Retweets  AnalysisDimension = AnalysisDimension("retweets")
 )
 
+// String return an AnalysisDimension underlying string data.
+func (ad AnalysisDimension) String() string { return string(ad) }
+
+var AllAnalysisDimensions = []string{
+	Likes.String(),
+	Comments.String(),
+	Favorites.String(),
+	Retweets.String(),
+}
+
 // Valid check if the string match one of the const values.
 func (ad AnalysisDimension) Valid() bool {
 	switch ad {
-	case Likes, Comments, Favorites, Retweets:
+	case Likes,
+		Comments,
+		Favorites,
+		Retweets:
 		return true
 	}
 
@@ -38,11 +52,12 @@ type AnalysisParam struct {
 func (ap AnalysisParam) Validate() error {
 	_, err := time.ParseDuration(ap.Duration)
 	if err != nil {
-		return fmt.Errorf("failed to parse duration : %w", err)
+		return fmt.Errorf("failed to parse duration: %w", err)
 	}
 
 	if !ap.Dimension.Valid() {
-		return fmt.Errorf(`invalid dimension "%s"`, string(ap.Dimension))
+		return fmt.Errorf(`invalid dimension "%s", valid dimensions are : "%s"`,
+			ap.Dimension.String(), strings.Join(AllAnalysisDimensions, ","))
 	}
 
 	return nil
@@ -50,45 +65,45 @@ func (ap AnalysisParam) Validate() error {
 
 // LikeDimensionPercentiles is percentiles values for dimension 'likes'.
 type LikeDimensionPercentiles struct {
-	LikesP50 uint64 `json:"likes_p50"`
-	LikesP90 uint64 `json:"likes_p90"`
-	LikesP99 uint64 `json:"likes_p99"`
+	LikesP50 int `json:"likes_p50"`
+	LikesP90 int `json:"likes_p90"`
+	LikesP99 int `json:"likes_p99"`
 }
 
 // CommentDimensionPercentiles is percentiles values for dimension 'comments'.
 type CommentDimensionPercentiles struct {
-	CommentsP50 uint64 `json:"comments_p50"`
-	CommentsP90 uint64 `json:"comments_p90"`
-	CommentsP99 uint64 `json:"comments_p99"`
+	CommentsP50 int `json:"comments_p50"`
+	CommentsP90 int `json:"comments_p90"`
+	CommentsP99 int `json:"comments_p99"`
 }
 
 // FavoriteDimensionPercentiles is percentiles values for dimension 'favorites'.
 type FavoriteDimensionPercentiles struct {
-	FavoritesP50 uint64 `json:"favorites_p50"`
-	FavoritesP90 uint64 `json:"favorites_p90"`
-	FavoritesP99 uint64 `json:"favorites_p99"`
+	FavoritesP50 int `json:"favorites_p50"`
+	FavoritesP90 int `json:"favorites_p90"`
+	FavoritesP99 int `json:"favorites_p99"`
 }
 
 // RetweetDimensionPercentiles is percentiles values for dimension 'retweets'.
 type RetweetDimensionPercentiles struct {
-	RetweetsP50 uint64 `json:"retweets_p50"`
-	RetweetsP90 uint64 `json:"retweets_p90"`
-	RetweetsP99 uint64 `json:"retweets_p99"`
+	RetweetsP50 int `json:"retweets_p50"`
+	RetweetsP90 int `json:"retweets_p90"`
+	RetweetsP99 int `json:"retweets_p99"`
 }
 
 // AnalysisResponse is the format of /analysis JSON response.
 type AnalysisResponse struct {
-	TotalPosts                    uint64 `json:"total_posts"`
-	MinTimestamp                  uint64 `json:"minimum_timestamp"`
-	MaxTimestamp                  uint64 `json:"maximum_timestamp"`
+	TotalPosts                    int `json:"total_posts"`
+	MinTimestamp                  int `json:"minimum_timestamp"`
+	MaxTimestamp                  int `json:"maximum_timestamp"`
 	*LikeDimensionPercentiles     `json:",omitempty"`
 	*CommentDimensionPercentiles  `json:",omitempty"`
 	*FavoriteDimensionPercentiles `json:",omitempty"`
 	*RetweetDimensionPercentiles  `json:",omitempty"`
 }
 
-// Fill set the required response fields for a particular dimension.
-func (ar *AnalysisResponse) Fill(dimension AnalysisDimension, datas [3]uint64) {
+// FillPercentiles set the required response fields for a particular dimension.
+func (ar *AnalysisResponse) FillPercentiles(dimension AnalysisDimension, datas [3]int) {
 	switch dimension {
 	case Likes:
 		ar.LikeDimensionPercentiles = &LikeDimensionPercentiles{
